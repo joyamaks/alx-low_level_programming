67a9962197ad2ad363c5299ac33bdf71a9f51f34
joyamaks <igboekwechiamaka@yahoo.com.com>
2022-07-08 15:12:34 -0700

    print num

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,9 +4,9 @@
  */
 void print_numbers(void);
 {
-	int c;
+	int x;
 
-	for (c = 0; c <= 9; c++)
-		_putchar(c + '0');
+	for (x = 0; x <= 9; x++)
+		_putchar(x + '0');
 	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,12 +4,12 @@
  */
 void print_most_numbers(void);
 {
-	int c;
+	int x;
 
-	for (c = 0; c < 10; c++)
+	for (x = 0; x <= 9; x++)
 	{
-		if (c != 2 && c != 4)
-			_putchar (c + '0');
+		if (x != 2 && x != 4)
+			_putchar (x + '0');
 	}
 	_putchar('\n');
 }
