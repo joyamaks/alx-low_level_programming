aa09d98b31d5fa3db931fb294f8b8048a74cf1f6
joyamaks <igboekwechiamaka@yahoo.com.com>
2022-07-12 13:33:53 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - function that returns the length of a string
+ * @s: char input
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 1;
+	while (*s != '\0')
+	{
+		s++;
+		i += 1;
+	}
+	return (i);
