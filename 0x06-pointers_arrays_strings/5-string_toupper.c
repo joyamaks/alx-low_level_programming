1e9f54bf096ff157334dd339b3430d8e9a301cf2
joyamaks <igboekwechiamaka@yahoo.com.com>
2022-07-15 10:50:34 -0700

    string to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - a function that changes all lower of a string to upper
+ * @p: input string
+ * Return: capitialized string
+ */
+char *string_toupper(char *p)
+{
+	int a = 0;
+
+	while (p[a])
+	{
+		if (p[a] >= 97 && p[a] <= 122)
+		{
+			p[a] -= 32;
+		}
+		a++;
+	}
+	return (p);
+}
