ee913b1d70ae021c03ae1c6e91db382ccd5d270d
joyamaks <igboekwechiamaka@yahoo.com.com>
2022-07-21 09:55:34 -0700

    is_prime_number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+int prime_checker(int n, int i);
+/**
+ * is_prime_number -returns 1 if the input integer is a prime number
+ * @n: input interger
+ * Return: Always 0 (Success)
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	else if (prime_checker(n, n / 2) > 0)
+		return (1);
+	else
+		return (0);
+}
+/**
+ * check_prime - Check if number is prime
+ * @n: number
+ * @i: the iteration times
+ * Return: 1 for prime or 0 composite
+ */
+int check_prime(int n, int i)
+{
+	if (n <= 1)
+		return (0);
+	else if (n % i == 0 && i > 1)
+		return (0);
+	else if ((n / i) < i)
+		return (1);
+	else
+		return (check_prime(n, i + 1));
+}
