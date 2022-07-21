072a1daf2b82a31d7fa29882c9a3f63d0775fe43
joyamaks <igboekwechiamaka@yahoo.com.com>
2022-07-21 09:34:55 -0700

    sqrt_recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+int _sqrt(int n, int i);
+/**
+ * _sqrt_recursion - Returns the natural square root of a number
+ * @n: number to calculate the natural square root
+ * Return: the natural square root
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
+/**
+ * _sqrt - checks if perfect square
+ * @n: input
+ * @i: counter
+ * Return: if square root
+ */
+int _sqrt(int n, int i)
+{
+	int sqrt = i * i;
+
+	if (sqrt > n)
+		return (-1);
+	else if (sqrt == n)
+		return (i);
+	else
+		return (_sqrt(n, i + 1));
+}
