c8e592b9fbec4dae64812a0f5c1cd758c79c8937
joyamaks <igboekwechiamaka@yahoo.com.com>
2022-07-21 13:04:44 -0700

    wildcmp

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 int prime_checker(int n, int i);
 /**
  * is_prime_number -returns 1 if the input integer is a prime number
@@ -7,12 +8,7 @@ int prime_checker(int n, int i);
  */
 int is_prime_number(int n)
 {
-	if (n <= 1)
-		return (0);
-	else if (prime_checker(n, n / 2) > 0)
-		return (1);
-	else
-		return (0);
+	return (check_prime(n, 1));
 }
 /**
  * check_prime - Check if number is prime
@@ -22,10 +18,12 @@ int is_prime_number(int n)
  */
 int check_prime(int n, int i)
 {
-	if (n % i == 0)
+	if (n % i == 0 && i > 1)
 		return (0);
-	else if (i == 1)
+	else if (n <= 1)
+		return (0);
+	else if ((n / i) < i)
 		return (1);
 	else
-		return (check_prime(n, i - 1));
+		return (check_prime(n, i + 1));
 }
