7791b5152fade3208d55752d5134512a2f24ab45
joyamaks <igboekwechiamaka@yahoo.com.com>
2022-07-21 07:43:49 -0700

    puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion -a function that prints a string
+ * @s: string to print
+ * Return: Always 0 (Success)
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+}
