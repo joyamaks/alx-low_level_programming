a32eafa81c4dfe7b505c3ca8ac1aa0990c006ecc
joyamaks <igboekwechiamaka@yahoo.com.com>
2022-07-26 12:13:18 -0700

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *  main -> this is a function to print its name
+ *  @argc: number of arguments typed
+ *  @argv: array pointing to arguements
+ *  Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+	return (0);
+}
