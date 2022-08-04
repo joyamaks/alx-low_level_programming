7e62a7f1db43762686cabcafd31b7aa6f874e137
joyamaks <igboekwechiamaka@yahoo.com.com>
2022-08-03 22:14:35 -0700

    main

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+/**
+ * op_add - returns the sum of a & b
+ * @a: interger value
+ * @b: interger value
+ * Return: 0
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub: return the difference of a & b
+ * @a: input
+ * @b: input
+ * Return: nothing
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiply
+ * @a: input
+ * @b: input
+ * Return: 0
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div: ....
+ * @a: ...
+ * @b: ...
+ * Return: ...
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - 5 functions
+ * @a: input one
+ * @b: input two
+ * Return: ..
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
