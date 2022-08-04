e404187fbe84304b176f5729a54e9ab4759be380
joyamaks <igboekwechiamaka@yahoo.com.com>
2022-08-03 20:53:06 -0700

    print

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name -function that prints name
+ * name: name of a person
+ * @f: pointer to print upper or lower
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
