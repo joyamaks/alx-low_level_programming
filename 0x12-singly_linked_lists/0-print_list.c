8f683f33f6ca7eb36b10a2ee067dff616171f360
joyamaks <igboekwechiamaka@yahoo.com.com>
2022-08-11 16:56:01 -0700

    update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,15 +10,13 @@
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
+	const list_t *current = h;
 
-	while (h != NULL)
+	while (current != NULL)
 	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-		else
-			printf("[%d] %s\n", h->len, h->str);
-
-		h = h->next;
+		printf("[%d] %s\n", current->len,
+				current->str != NULL ? current->str : "(nil)");
+		current = current->next;
 		count++;
 	}
 	return (count);
